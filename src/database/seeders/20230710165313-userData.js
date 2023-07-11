f4caecaf5908a4cbbaa623591056bb63948cd22e
Nicolain12 <109926340+Nicolain12@users.noreply.github.com>
2023-07-11 13:46:02 -0300

    cars models seeders done

--- src/database/seeders/20230710165313-userData.js
+++ src/database/seeders/20230710165313-userData.js
@@ -10,7 +10,7 @@ module.exports = {
       surname: 'Lain',
       email: 'nicolas@mail.com ',
       birthdate: '2002-10-12',
-      phoneNumber: [['+54'],['1133615533']],
+      phoneNumber: JSON.stringify([['+54'],['1133615533']]),
       password: '$2a$10$2Q4jJKJnTT4bxjCPuYuYbOz7hVQzWe/.tJY9fY4EcqIbpaGURExqu',
       createdAt: new Date(),
       updatedAt: new Date()
@@ -21,7 +21,7 @@ module.exports = {
       surname: 'Pardo',
       email: 'maxipar@mail.com ',
       birthdate: '2001-6-10',
-      phoneNumber: [['+54'],['1126790946']],
+      phoneNumber: JSON.stringify([['+54'],['1126790946']]),
       password: '$2a$10$H8VnhEq2F07ayCGBOZhu2uTOSH6WcarPJ22gaKhAqOPig4YyBK8bu',
       createdAt: new Date(),
       updatedAt: new Date()
