9cb55ce3ea2da8345adbfe15c375992d5ed07458
Nicolain12 <109926340+Nicolain12@users.noreply.github.com>
2023-07-11 18:24:20 -0300

    list brands, models and cars

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const path = require('path')
+const cors = require('cors');
 const app = express();
+//routes requires
+const usersApi = require('./routes/usersRoutes')
+const carsApi = require('./routes/carsRoutes')
 
 app.use(express.json())
+app.use(cors());
 
 const publicPath = path.join(__dirname, '../public')
 app.use(express.static(publicPath))
 
+//Listener
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`)
     console.log('http://localhost:3000')
 }) 
+
+//Routes
+app.use('/users', usersApi)
+app.use('/cars', carsApi)
